5a6e95f4595672de0245f4608752a6129978ee57
pmerlyn <paul@paulmerlyn.com>
2021-10-27 19:22:03 -0700

    Formatting errors so they display what we want the user to see

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer, ApolloError } = require('apollo-server')
 const SessionAPI = require('./datasources/sessions')
 const SpeakerAPI = require('./datasources/speakers')
 
@@ -19,7 +19,16 @@ const dataSources = () => {
 }
 
 const server = new ApolloServer({
-  typeDefs, resolvers, dataSources, introspection: true // set introspection to false to prevent its use in the Apollo Studio sandbox
+  typeDefs,
+  resolvers,
+  dataSources,
+  introspection: true, // set introspection to false to prevent its use in the Apollo Studio sandbox
+  // can also add debug: false to prevent full stacktraces showing up with production errors (useful in dev though!)
+  formatError: (err) => {
+    if (err.extensions.code === 'INTERNAL_SERVER_ERROR') {
+      return new ApolloError('We are having technical difficulties', 'ERROR', { token: 'uniqueToken' })
+    }
+  }
 })
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
